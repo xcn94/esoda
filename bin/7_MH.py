2512e34e3edfa70d807701661cbcbdf32df7ca46
xcn94 <xcn941202@gmail.com>
2019-06-17 04:51:58 +0800

    version1.1 only with gpt2

--- bin/7_MH.py
+++ bin/7_MH.py
@@ -1,5 +1,6 @@
 import torch
 import random
+import copy
 from string import punctuation
 from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
 
@@ -16,6 +17,9 @@ import math
 K = TypeVar('K')
 V = TypeVar('V')
 MEDIUM_MODEL = '../gpt2-pytorch-pretrained'
+
+gpus = [0, 1]
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class LRUCache(Generic[K, V]):
@@ -48,27 +52,26 @@ class LRUCache(Generic[K, V]):
 def random_sample(logits: torch.Tensor, temperature: float = 1.0) -> int:
     d = torch.distributions.Categorical(logits=logits / temperature)
     return d.sample().item()
-def compute_LL_oneword(model, pre_sentence, next_word):
+def compute_LL_onetoken(model, pre_tokens, next_token):
+    pre_sentence = gpt2_tokenizer.decode(pre_tokens)
     logits = model.predict(pre_sentence)
     probabilities = torch.nn.functional.softmax(logits, dim=-1)
-    ids = model.tokenizer.encode(next_word)
-    return probabilities[ids].tolist()[0]
+    return probabilities[next_token].tolist()
 def sentence2gptlist(sentence):
     from itertools import groupby
     from string import punctuation
 
     sentence_list = [''.join(list(g)).strip() for k, g in groupby(sentence, key=lambda x: x.isalpha() or x == '\'')]
-    print(sentence_list)
     new_list = []
     for ids, word in enumerate(sentence_list):
         if word not in punctuation and ids != 0:
             word = ' ' + word
         if word != '':
             new_list.append(word)
-
+    print('new_list: ', new_list)
     return new_list
 def compute_LL(model, sentence:str):
-    sentence_list = sentence2gptlist(sentence)
+    # sentence_list = sentence2gptlist(sentence)
     # logLL = 0.0
     # # 从第二个词开始算句子的概率。
     # if len(sentence_list) != 0:
@@ -78,15 +81,16 @@ def compute_LL(model, sentence:str):
     #         logLL += math.log(prob)
     #         pre_sentence += word
     # assert pre_sentence == sentence
+    tokens_list = gpt2_tokenizer.encode(sentence)
     logLL = 1.0
     # 从第二个词开始算句子的概率。
-    if len(sentence_list) != 0:
-        pre_sentence = sentence_list[0]
-        for word in sentence_list[1:]:
-            prob = compute_LL_oneword(model, pre_sentence, word)
-            logLL *= prob
-            pre_sentence += word
-    assert pre_sentence == sentence
+    if len(tokens_list) != 0:
+        pre_token = tokens_list[0]
+        for ids, token in enumerate(tokens_list):
+            if ids > 0:
+                prob = compute_LL_onetoken(model, tokens_list[:ids], token)
+                logLL *= prob
+    # assert tokens_list == sentence
     return logLL
 def compute_gptLL(sentence:str):
     return compute_LL(model_345M, sentence)
@@ -108,7 +112,7 @@ class GPT2LanguageModel(LanguageModel):
         if model_name == '117M':
             self.model = GPT2LMHeadModel.from_pretrained('gpt2')
         elif model_name == '345M':
-            self.model = GPT2LMHeadModel.from_pretrained(MEDIUM_MODEL)
+            self.model = GPT2LMHeadModel.from_pretrained(MEDIUM_MODEL).cuda()
         else:
             exit("model name not found")
 
@@ -133,7 +137,7 @@ class GPT2LanguageModel(LanguageModel):
         else:
             token_ids = self.tokenizer.encode(previous) + self.tokenizer.encode(next)
 
-        inputs = torch.LongTensor([token_ids])
+        inputs = torch.LongTensor([token_ids]).to(device)
 
         logits, present = self.model(inputs, past=past)
         logits = logits[0, -1]
@@ -173,18 +177,18 @@ model_345M = GPT2LanguageModel(model_name='345M')
 gpt2_tokenizer = GPT2Tokenizer.from_pretrained(MEDIUM_MODEL)
 
 # bert拜拜 你真不好用
-bert_path = '/root/xcn/bert_pytorch'
-bert_tokenizer = BertTokenizer.from_pretrained(bert_path)
-bert_model = BertForMaskedLM.from_pretrained(bert_path)
+# bert_path = '/root/xcn/bert_pytorch'
+# bert_tokenizer = BertTokenizer.from_pretrained(bert_path)
+# bert_model = BertForMaskedLM.from_pretrained(bert_path)
+# bert_token = bert_tokenizer.tokenize(sentence)
 
 
 
-sentence = 'However, it is too late'
-bert_token = bert_tokenizer.tokenize(sentence)
+sentence = 'we types proposals'
+keywords = gpt2_tokenizer.encode(sentence)
 gpt2_token = [gpt2_tokenizer.decoder[ids].replace('\u0120', '')
               for ids in gpt2_tokenizer.encode(sentence)]
-print(bert_token)
-print(gpt2_token)
+
 
 
 p_insert = 1/3
@@ -196,12 +200,13 @@ class PiOrigin:
     def LM_propility(self, x) -> float:
         return x
 class PiKeyWords(PiOrigin):
-    def compute_pi(self, x, keywords:list=[]):
-        for keyword in keywords:
-            if keyword not in self.x:
-                return 0.0
-            else:
-                return self.LM_propility(self.x)
+    def __init__(self, keywords=[]):
+        self.keywords = keywords
+    def compute_pi(self, x:list=[]):
+        for keyword in self.keywords:
+            if keyword not in x:
+                return 0
+        return 1
 class PiFactory:
     @staticmethod
     def typename(name):
@@ -210,8 +215,7 @@ class PiFactory:
         else:
             return None
 
-
-pi_keywords = PiFactory.typename('keywords')
+pi = PiKeyWords(keywords)
 
 
 def random_index(prob_list:list) -> int:
@@ -229,56 +233,62 @@ def random_index(prob_list:list) -> int:
 def Candidate_Generator():
     pass
 
-def G_replace(x:str, ids, isInsert=False):
+def G_replace(x:str, pos, isInsert=False, isDelete=False, *ids):
     # ids的位置是根据tokenize后的位置计算的，而不是x的位置！
-    bert_tokens = bert_tokenizer.tokenize(x)
+    # bert_tokens = bert_tokenizer.tokenize(x)
+    gpt2_tokens = gpt2_tokenizer.encode(x)
+    # print(gpt2_tokens)
+    x_split = gpt2_tokenizer.decode(gpt2_tokens[:pos])
+    # print(x_split)
+    logits = model_345M.predict(x_split)
+    candidate_logits, candidate_ids = logits.topk(10)
+    candidate_ids = candidate_ids.tolist()
+    if isDelete:
+        assert(len(ids) == 1)
+        candidate_ids = list(ids) + candidate_ids
+    # print(candidate_ids)
+    candidate_tokenslist = []
     if isInsert:
-        bert_tokens.insert(ids, '[MASK]')
+        gpt2_tokens.insert(pos, 0)
+        for ids in candidate_ids:
+            gpt2_tokens[pos] = ids
+            candidate_tokenslist.append(copy.deepcopy(gpt2_tokens))
     else:
-        bert_tokens[ids] = '[MASK]'
+        for ids in candidate_ids:
+            gpt2_tokens[pos] = ids
+            candidate_tokenslist.append(copy.deepcopy(gpt2_tokens))
+    # print(candidate_tokenslist)
+    candidate_sentencelist = [gpt2_tokenizer.decode(x) for x in candidate_tokenslist]
+    candidate_probilities = [compute_gptLL(gpt2_tokenizer.decode(x)) for x in candidate_tokenslist]
+    candidate_probilities = [x/sum(candidate_probilities) for x in candidate_probilities]
+    # print(candidate_sentencelist)
+    # print(candidate_probilities)
+    ret_index = random_index(candidate_probilities)
 
-    tokens_ids = bert_tokenizer.convert_tokens_to_ids(bert_tokens)
-    segments_ids = [0] * len(tokens_ids)
-
-    tokens_tensor = torch.tensor([tokens_ids])
-    segments_tensor = torch.tensor([segments_ids])
-
-    bert_predictions = bert_model(tokens_tensor, segments_tensor)
-    bert_probilities = torch.nn.functional.softmax(bert_predictions[0, ids], dim=-1)
-    ret_index = random_index(bert_probilities.tolist())
-    tokens_ids[ids] = ret_index
-
-    sentence_list = bert_tokenizer.convert_ids_to_tokens(tokens_ids)
-    new_list = []
-    for ids, word in enumerate(sentence_list):
-        if word not in punctuation and ids != 0:
-            word = ' ' + word
-        if word != '':
-            new_list.append(word)
-    sentence = ''.join(new_list)
-    return sentence, bert_probilities[ret_index], bert_probilities
-def G_delete(x:str, idx):
-    bert_tokens = bert_tokenizer.tokenize(x)
-    deleted_word = bert_tokens.pop(idx)
-    sentence_list = bert_tokens
-    new_list = []
-    for ids, word in enumerate(sentence_list):
-        if word not in punctuation and ids != 0:
-            word = ' ' + word
-        if word != '':
-            new_list.append(word)
-    sentence = ''.join(new_list)
-    deleted_ids = bert_tokenizer.convert_tokens_to_ids([deleted_word])
+    return candidate_sentencelist[ret_index], candidate_probilities[ret_index], \
+           candidate_probilities, candidate_tokenslist
+def G_delete(x:str, pos):
+    gpt2_tokens = gpt2_tokenizer.encode(x)
+    deleted_ids = gpt2_tokens.pop(pos)
+    sentence = gpt2_tokenizer.decode(gpt2_tokens)
     return sentence, deleted_ids
 
 # print(G_delete(sentence, 3))
-# G_replace(sentence, 3)
+# G_replace(sentence, 3, True, True, 320)
 def A_replace(x:str, idx) -> str:
-    return G_replace(x, idx)[0]
+    x_prop = G_replace(x, idx)[0]
+    x_prop_tokens = gpt2_tokenizer.encode(x_prop)
+    if pi.compute_pi(x_prop_tokens):
+        return x_prop
+    else:
+        return x
 
 def A_insert(x:str, idx) -> str:
-    x_prop, g_prob, _ = G_replace(x, idx, isInsert=True)
-    accept_prob = (p_delete/p_insert) * (compute_gptLL(x_prop)/compute_gptLL(x)) * (1/g_prob)
+    x_prop, g_prob, _, _ = G_replace(x, idx, isInsert=True)
+    x_tokens = gpt2_tokenizer.encode(x)
+    x_prop_tokens = gpt2_tokenizer.encode(x_prop)
+    accept_prob = (p_delete/p_insert) * (compute_gptLL(x_prop)/compute_gptLL(x)) * (1/g_prob) \
+                  * pi.compute_pi(x_prop_tokens) / pi.compute_pi(x_tokens)
     # print(accept_prob)
 
     randnum = random.random()
@@ -289,9 +299,11 @@ def A_insert(x:str, idx) -> str:
 
 def A_delete(x:str, idx) -> str:
     x_prop, deleted_ids = G_delete(x, idx)
-    #  这个LM模型取了对数，绝对值越小概率越高，所以第二项取个倒
+    x_tokens = gpt2_tokenizer.encode(x)
+    x_prop_tokens = gpt2_tokenizer.encode(x_prop)
     accept_prob = (p_insert/p_delete) * (compute_gptLL(x_prop)/compute_gptLL(x)) * \
-                  G_replace(x_prop, idx, isInsert=True)[2][deleted_ids]
+                  G_replace(x_prop, idx, True, True, deleted_ids)[2][0] * \
+                  pi.compute_pi(x_prop_tokens) / pi.compute_pi(x_tokens)
     # print(accept_prob)
     randnum = random.random()
     if randnum < accept_prob:
@@ -302,21 +314,25 @@ def A_delete(x:str, idx) -> str:
 # ret = A_insert(sentence, 3)
 # print(ret)
 
+
+# 执行部分
 proposal_prob = [1/3, 1/3, 1/3]
 
-for _ in tqdm(range(200)):
+for _ in tqdm(range(100)):
     proposal = random_index(proposal_prob)
-    bert_tokens = bert_tokenizer.tokenize(sentence)
-    tokens_len = len(bert_tokens)
+    gpt2_tokens = gpt2_tokenizer.encode(sentence)
+    tokens_len = len(gpt2_tokens)
     position_prob = [1/tokens_len for _ in range(tokens_len)]
     position = random_index(position_prob)
     # 由于单向语言模型问题，Gibbs Sampling无法计算带suffix的LM概率，因此replace和insert操作都不能对第一个单词进行替换
-    # delete这种憨憨操作就不存在这个问题
+    # 同时不能删除第一个节点，因为这样会输入空的tensor
     if proposal == 0 and position > 0:
         sentence = A_replace(sentence, position)
     elif proposal == 1:
         sentence = A_insert(sentence, position+1)
-    elif proposal == 2:
+    elif proposal == 2 and position > 0:
         sentence = A_delete(sentence, position)
+    print(sentence)
 
-print(sentence)
+
+# 我觉得为了更好的效果需要重写candidate selector。
